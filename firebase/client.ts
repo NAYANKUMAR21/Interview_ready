8e696b535d37ac221263de65ed281f1e221457db
Nayan Kumar <naayaankumar@gmail.com>
2025-04-15 15:07:46 +0530

    firestore Updated

--- firebase/client.ts
+++ firebase/client.ts
@@ -8,15 +8,15 @@ import { getFirestore } from 'firebase/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later,p measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBjt6hTTepIE1OtUwlKsV-cm2X6ZfKn4Zk',
-  authDomain: 'interview-project-642f0.firebaseapp.com',
-  projectId: 'interview-project-642f0',
-  storageBucket: 'interview-project-642f0.firebasestorage.app',
-  messagingSenderId: '699491935391',
-  appId: '1:699491935391:web:d1b94ce3568f2d0a902eaa',
-  measurementId: 'G-4B5ZJ69YHJ',
+  apiKey: process.env.NEXT_PUBLIC_APIKEY!,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID!,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGESENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID!,
 };
 
 // Initialize Firebase
